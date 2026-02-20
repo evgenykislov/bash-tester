33104f5a6a0212cbb338e621b68267a42eeaac50
Evgeny Kislov <dev@evgenykislov.com>
2026-02-20 10:28:39 +0300

    Добавлен вывод длительности исполнения теста.

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -10,11 +10,12 @@ set +e
 
 TEST_NAME=$(basename $1)
 TEST_RES=0
+TEST_START_TIME=$(date +%s%3N)
 
 # Подключаем сам тестовый скрипт
 source $1
 
-echo "Test '${TEST_NAME}' running ..."
+echo "[ RUN      ] ${TEST_NAME}"
 
 if [[ $(type -t setup) == "function" ]]; then
   setup
@@ -40,8 +41,11 @@ if [[ $(type -t teardown) == "function" ]]; then
   fi
 fi
 
+
+TEST_FINISH_TIME=$(date +%s%3N)
+TEST_DURATION=$((TEST_FINISH_TIME - TEST_START_TIME))
 if [[ ${TEST_RES} == 0 ]]; then
-  echo "  Tesk OK"
+  echo "[       OK ] ${TEST_NAME} (${TEST_DURATION} ms)"
 fi
 
 exit ${TEST_RES}
